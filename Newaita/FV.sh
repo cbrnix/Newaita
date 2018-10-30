0f4f4dad8f6560268af52bf571be67d0739227e8
cbrnix <ghostdmn2@gmail.com>
2018-10-30 21:10:20 +0300

    Added:
    New places variant - Mint
    
    frostwire
    
    Restyle/Make-up:
    
    blogilo

--- Newaita/FV.sh
+++ Newaita/FV.sh
@@ -2,36 +2,39 @@
 #!/bin/bash
 
 echo "choose your folders variant
-1) Blue
-2) Manjaro
-3) Ubuntu
-4) Default
-5) Darknight
-6) Bluegray
-7) Aqua
-8) Gray
-9) Quit"
+1) Aqua
+2) Blue
+3) Bluegray
+4) Darknight
+5) Gray
+6) Manjaro
+7) Mint
+8) Ubuntu
+9) Default
+10) Quit"
 
 read n
 
 case $n in
 
-1) rm -rf ./places
-ln -s ./.places-blue ./places;;
-2)rm -rf ./places
-ln -s ./.places-manjaro ./places;;
-3)rm -rf ./places
-ln -s ./.places-ubuntu ./places;;
-4)rm -rf ./places
-ln -s ./.places-default ./places;;
-5)rm -rf ./places
-ln -s ./.places-darknight ./places;;
-6)rm -rf ./places
-ln -s ./.places-bluegray ./places;;
-7)rm -rf ./places
+1)rm -rf ./places
 ln -s ./.places-aqua ./places;;
-8)rm -rf ./places
+2) rm -rf ./places
+ln -s ./.places-blue ./places;;
+3)rm -rf ./places
+ln -s ./.places-bluegray ./places;;
+4)rm -rf ./places
+ln -s ./.places-darknight ./places;;
+5)rm -rf ./places
 ln -s ./.places-gray ./places;;
-9)break;;
+6)rm -rf ./places
+ln -s ./.places-manjaro ./places;;
+7)rm -rf ./places
+ln -s ./.places-mint ./places;;
+8)rm -rf ./places
+ln -s ./.places-ubuntu ./places;;
+9)rm -rf ./places
+ln -s ./.places-default ./places;;
+10)break;;
 *)invalid option;;
 esac
